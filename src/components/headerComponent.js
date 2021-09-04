41f99f7ff15aacc7b3746e49ae688dc36087ef0c
Sudhanshu Kulkarni <sudhanshu.kulkarni2105@gmail.com>
2021-09-04 11:22:00 +0530

    router with navbar

--- src/components/headerComponent.js
+++ src/components/headerComponent.js
@@ -1,13 +1,58 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand, Jumbotron } from 'reactstrap';
-
+import { Navbar, NavbarBrand, Jumbotron, Nav,NavbarToggler,NavItem,Collapse } from 'reactstrap';
+import {NavLink} from 'react-router-dom';
 class Header extends Component {
+
+
+  constructor(props){
+    super(props);
+    this.state={
+      isNavOpen: false
+    };
+    this.toggleNav=this.toggleNav.bind(this);
+  }
+  toggleNav(){
+    this.setState(
+      {
+        isNavOpen: !this.state.isNavOpen
+      }
+    );
+  }
   render() {
     return(
     <React.Fragment>
-      <Navbar dark>
+      <Navbar dark expand="md">  
+      {/* expand only for small to medium */}
         <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          <NavbarToggler onClick={this.toggleNav}/>
+            <NavbarBrand className="mr-auto"href="/">
+              <img src="assets/image/images/logo.png" height="30" width="41"></img>
+            </NavbarBrand>
+
+          <Collapse isOpen ={this.state.isNavOpen} navbar>
+            <Nav navbar>
+              <NavItem>
+                <NavLink className="nav-link" to="/home">
+                  <span className="fa fa-home fa-lg">Home</span>
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink className="nav-link" to="/aboutus">
+                  <span className="fa fa-info fa-lg">About us</span>
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink className="nav-link" to="/menu">
+                  <span className="fa fa-list fa-lg">Menu</span>
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink className="nav-link" to="/contactus">
+                  <span className="fa fa-address-card fa-lg">Contact us</span>
+                </NavLink>
+              </NavItem>
+            </Nav>
+           </Collapse>
         </div>
       </Navbar>
       <Jumbotron >
